cdcbc646372990d2e7e488ecab429be693d78595
Jesus Zenteno <jedrzr24@gmail.com>
2025-07-13 18:51:47 -0600

    added new visualization graphs, date bug in dashboard fixed

--- static/js/flash_message.js
+++ static/js/flash_message.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Hide flash messages after 4 seconds
   const flashDiv = document.getElementById('flash-messages');
   if (flashDiv) {
     setTimeout(() => {
       flashDiv.style.display = 'none';
-    }, 4000); // 4 segundos
+    }, 4000); // 4 secs
   }
 });
